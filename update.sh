cec742ef0f2d10f9c947b936e4fa008a776deb84
Stefan Schärmeli <schaermu@gmail.com>
2023-09-29 11:02:54 +0200

    feat: adds reup command to maintenance shell script

--- update.sh
+++ update.sh
@@ -20,6 +20,13 @@ update_all() {
 	done
 }
 
+reup_all() {
+	for dir in */ ; do
+		[ -L "${d%/}" ] && continue
+		reup "$dir"
+	done
+}
+
 update() {
 	if [ -d "$1" ]; then
 		pushd $1
@@ -33,15 +40,28 @@ update() {
 	fi
 }
 
+reup() {
+	if [ -d "$1" ]; then
+		pushd $1
+		echo "${BLUE}[`date`]${NORM} restarting '$1'."
+	        docker compose up -d --force-recreate
+		echo "${BLUE}[`date`]${NORM} '$1' restarted."
+		popd
+	else
+		echo "${BLUE}[`date`]${RED} '$1' not found!"
+	fi
+}
 
-if [ "$#" -ne 1 ]; then
-	echo "${BLUE}[`date`]${RED} Please state which stack to update or use '--all' to patch them all."
+if [ "$#" -ne 2 ]; then
+	echo "${BLUE}[`date`]${RED} Please state which action [update/reup] to apply to which stack or use '--all' to apply action to all of them."
         exit
 fi
 
-case $1 in
-        "--all") update_all ;;
-        *) update $1 ;;
+case "$1:$2" in
+"update:--all") update_all ;;
+"update:") update $2 ;;
+"reup:--all") reup_all ;;
+"reup:") reup $2 ;;
 esac
 
 # clean up old images after updating
